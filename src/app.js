1fac7c704811b6d53a849a3c7d8d9ca6154c1537
Shivam Maisuriya <maisuriyas89@gmail.com>
2024-12-03 21:46:22 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));// to enable json parsing
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to enable url encoded parsing
+app.use(express.static("public")); // to serve static files from the public folder
+app.use(cookieParser()); // to parse cookies
+
+export { app };
